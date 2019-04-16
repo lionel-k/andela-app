84466fccc33886fdee2c410eb1605f664d38ad77
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2019-04-16 18:00:36 +0200

    add context articles controller specs

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,57 +1,62 @@
 require 'rails_helper'
 
 RSpec.describe ArticlesController, type: :controller do
-  it 'creates an album with valid data' do
-    user = User.create(name: 'toto')
-    post :create, params: {
-                        article: {
-                                content: 'awesome content',
-                                title: 'title',
-                                user_id: user.id
-                              }
-                        }
+  describe "GET #create" do
+    it 'creates an album with valid data' do
+      user = User.create(name: 'toto')
+      post :create, params: {
+                          article: {
+                                  content: 'awesome content',
+                                  title: 'title',
+                                  user_id: user.id
+                                }
+                          }
 
-    json = JSON.parse(response.body)
-    expect(json['content']).to eq('awesome content')
-    expect(json['title']).to eq('title')
-    expect(json['user_id']).to eq(user.id)
-    expect(response).to have_http_status(200)
+      json = JSON.parse(response.body)
+      expect(json['content']).to eq('awesome content')
+      expect(json['title']).to eq('title')
+      expect(json['user_id']).to eq(user.id)
+      expect(response).to have_http_status(200)
+    end
   end
 
-
-  it 'deletes an article' do
-    user = User.create(name: 'toto')
-    article = Article.create(title: 'title',
-      content: 'content',
-      user: user)
-    article_id = article.id
-
-    delete 'destroy', params: { id: article_id }
-
-    expect(response.body).to be_empty
-    expect(response).to have_http_status(204)
-    expect(Article.ids).not_to include article_id
-    end
-
-  it 'shows all articles' do
-    user = User.create!(name: 'roger')
-    another_user = User.create!(name: 'other_user')
-    5.times do |i|
-      Article.create!(title: "title-#{i}",
-        content: "content-#{i}",
+  describe "DELETE #destroy" do
+    it 'deletes an article' do
+      user = User.create(name: 'toto')
+      article = Article.create(title: 'title',
+        content: 'content',
         user: user)
+      article_id = article.id
+
+      delete 'destroy', params: { id: article_id }
+
+      expect(response.body).to be_empty
+      expect(response).to have_http_status(204)
+      expect(Article.ids).not_to include article_id
     end
+  end
 
-    5.times do |i|
-      Article.create!(title: "title-#{i}",
-        content: "content-#{i}",
-        user: another_user)
+  describe 'GET #index' do
+    it 'shows all articles' do
+      user = User.create!(name: 'roger')
+      another_user = User.create!(name: 'other_user')
+      5.times do |i|
+        Article.create!(title: "title-#{i}",
+          content: "content-#{i}",
+          user: user)
+      end
+
+      5.times do |i|
+        Article.create!(title: "title-#{i}",
+          content: "content-#{i}",
+          user: another_user)
+      end
+
+      get :index
+
+      json = JSON.parse(response.body)
+      expect(json.size).to eq(10)
+      expect(response).to have_http_status(200)
     end
-
-    get :index
-
-    json = JSON.parse(response.body)
-    expect(json.size).to eq(10)
-    expect(response).to have_http_status(200)
   end
 end
